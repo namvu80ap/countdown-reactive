720b7d317be81c448e004979bdd3de95fc68271e
namvu80ap <namvu80ap@yahoo.com>
2016-12-12 18:41:28 +0900

    Init

---
+++ src/main/java/com/namvu/dev/CountdownReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.namvu.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountdownReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountdownReactiveApplication.class, args);
+	}
+}
---
+++ src/main/java/com/namvu/dev/reactive/services/CountdownService.java
@@ -0,0 +1,14 @@
+package com.namvu.dev.reactive.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by v_nam on 2016/11/24.
+ */
+@Service
+public class CountdownService {
+    private static final Logger log = LoggerFactory.getLogger(CountdownService.class);
+
+}
